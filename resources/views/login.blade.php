a1a41ad837a69de60b2b5815b1233dbdf2fb96ce
alfii <alfianmaulana356@gmail.com>
2024-10-23 11:24:49 +0700

    update ler

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
     <form id="loginForm" class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
